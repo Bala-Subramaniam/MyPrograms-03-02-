4c87c9c98cda892f702a8bc66368e136b95d5b59
Bala Subramaniam <79137815+Bala-Subramaniam@users.noreply.github.com>
2022-02-06 23:30:29 +0530

    Update Program58.java

--- Program58.java
+++ Program58.java
@@ -8,12 +8,12 @@ public class Program58 {
 		if (factNum == 0) {
 			return 1; // 0! = 1
 		} else {
-			return (factNum * fact(factNum-1)); //
-		}
+			return (factNum * fact(factNum-1)); //factNum(initial number-5) * fact(factNum-) ==> (5-1) iterates again and again 5 * 4 * 3 * 2 * 1.		}
 	}
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		sc.close();
 		int factNum = sc.nextInt();
 		System.out.println("The factorial value is "+ fact(factNum));
 
