8bdcc0a85fa1ce3a23af794ecdc3e0a78556cf31
Bala Subramaniam <79137815+Bala-Subramaniam@users.noreply.github.com>
2022-02-06 23:34:42 +0530

    I did this program on 06/02/2022.

--- Program57.java
+++ Program57.java
@@ -13,9 +13,9 @@ public class Program57 {
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner (System.in);
-		sc.close();
 		System.out.println("Enter the Number of Sum of Natural numbers needed : ");
 		int natNum = sc.nextInt();
+		sc.close();
 		System.out.println("The sum of all natural number is "+naturalNum(natNum));
 
 	}
